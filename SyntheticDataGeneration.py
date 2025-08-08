ae59d1b86387960e21cca61aad55449b30901c16
Rahul Kushwaha <73552504+rahul8kushwaha@users.noreply.github.com>
2025-08-08 16:46:58 +0530

    Update SyntheticDataGeneration.py enchancement

--- SyntheticDataGeneration.py
+++ SyntheticDataGeneration.py
@@ -2,5 +2,6 @@ import torch
 def get_synthetic_data(n):
     x=torch.linspace(0,1,n)
     x=torch.randint(1,2,(1,))*(x-torch.rand(1))*(x+torch.rand(1))*(x-torch.rand(1))*(x-torch.rand(1))+\
-        (torch.rand(1)/10)*torch.randn(n)+(torch.rand(1)/10)*torch.rand(n)+0.1*torch.sin(2*torch.pi*x*6+torch.pi/6)+0.1*torch.sin(2*torch.pi*x*3)
+        (torch.rand(1)/10)*torch.randn(n)+(torch.rand(1)/10)*torch.rand(n)+\
+            torch.sum(torch.vstack([(0.05+0.1*torch.rand(1))*torch.sin(2*torch.pi*x*torch.randint(5,8,(1,))+torch.rand(1)*torch.pi) for _ in range(3)]),dim=0)
     return 100*x
