28ce900458068b33085c9693942b48ff789e3b1e
Rahul <rahul8kushwaha@gmail.com>
2026-03-14 14:27:01 +0530

    third commit

--- get_stock_data.py
+++ get_stock_data.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import torch
 def get_stock_data(PATH):
     # function for fetching stock data via yahoo finance
+    # can be changes later to get more fine grained data
     data=pd.read_csv(PATH)
     data.columns=list(map(lambda a:a.lower().lstrip().rstrip(),data.columns))
     data.date=data.date.apply(lambda a:datetime.strptime(a,'%d-%b-%Y'))
